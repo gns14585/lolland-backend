541c3430748efb7d5b3f5547171477be7467f4d7
LeeJungHoon <gns14585@naver.com>
2024-06-22 02:52:51 +0900

    오류 수정 AppConfig.java 파일에 ApacheHttpClient 해당 코드 추가

--- src/main/java/com/example/lollandback/config/AppConfig.java
+++ src/main/java/com/example/lollandback/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -24,6 +25,7 @@ public class AppConfig {
         S3Client s3 = S3Client.builder()
                 .region(Region.AP_NORTHEAST_2)
                 .credentialsProvider(provider)
+                .httpClient(ApacheHttpClient.builder().build())  // Apache HTTP 클라이언트를 명시적으로 설정
                 .build();
 
         return s3;
